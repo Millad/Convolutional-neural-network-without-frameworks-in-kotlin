7b9052e4ad101f2effc542840ac860e8c372eaa8
Millad <milladd@gmail.com>
2020-10-24 04:55:36 +0200

    fjernet referanse til openCV i app testene

--- src/test/kotlin/com/dagdoni/millad/deeplearning/AppTest.kt
+++ src/test/kotlin/com/dagdoni/millad/deeplearning/AppTest.kt
@@ -2,25 +2,16 @@ package com.dagdoni.millad.deeplearning
 
 import io.mockk.every
 import io.mockk.mockk
-import nu.pattern.OpenCV
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.opencv.core.CvType
-import org.opencv.core.Mat
 
 
 class AppTest {
 
-    @BeforeEach
-    fun setup(){
-        OpenCV.loadLocally()
-    }
-
     @Test fun `skal kunne fa tilbake riktig matrise fra openCV Adapter`(){
         // GITT
         val bilde:Bilde = mockk<Bilde>("talletEn.png")
-        val app:App = App(bilde)
+        val app = App(bilde)
 
         // NAAR
         every { bilde.somMatrise() } returns Matrise(3,3)
@@ -32,7 +23,7 @@ class AppTest {
 
     @Test fun `skal kunne returnere en tom matrise naar bildet er ikke funnet`() {
         // GITT
-        val app:App = App(Bilde("feil navn"))
+        val app = App(Bilde("feil navn"))
 
         // NAAR
         val matrise: Matrise = app.hentMatriseFraBildet()
