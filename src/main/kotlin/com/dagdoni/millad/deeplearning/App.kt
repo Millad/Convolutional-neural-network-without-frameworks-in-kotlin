8e779e7fc4b70e000a87d3766a7f161b4b4930c8
Millad Dagdoni <milladd@gmail.com>
2021-02-07 18:18:34 +0100

    skrivefeil av variabler for utregning av feilverdier i App.kt

--- src/main/kotlin/com/dagdoni/millad/deeplearning/App.kt
+++ src/main/kotlin/com/dagdoni/millad/deeplearning/App.kt
@@ -24,15 +24,15 @@ object App {
             // Verifiser riktig bildet
             val verifisering_lag_1 = vertikalBildeMatrise.conv(Matrise.hentVertikalKernel(kernelStorrelse))
             val verifisering_lag_2 = Matrise.relu(dot(verifisering_lag_1, vekterTrent))
-            val feil_prosent_gitt_riktig_bilde = (verifisering_lag_2 - mål).pow(2)
-            println("Feil for riktig bilde       : ${feil_prosent_gitt_riktig_bilde}")
+            val feil_gitt_riktig_bilde = (verifisering_lag_2 - mål).pow(2)
+            println("Feil for riktig bilde       : ${feil_gitt_riktig_bilde}")
 
             // Verifiser feil bilde med høy error
             val horisontalBilde = Bilde("horizontalLinje.png")
             val feil_bilde_lag_1 = horisontalBilde.somMatrise().conv(Matrise.hentHorizontalKernel(kernelStorrelse))
             val feil_bilde_lag_2 = Matrise.relu(dot(feil_bilde_lag_1, vekterTrent))
-            val feil_prosent_gitt_feil_bilde = (feil_bilde_lag_2 - mål).pow(2)
-            println("Feil for et helt annet bilde: ${feil_prosent_gitt_feil_bilde}")
+            val feil_gitt_feil_bilde = (feil_bilde_lag_2 - mål).pow(2)
+            println("Feil for et helt annet bilde: ${feil_gitt_feil_bilde}")
 
             println("Programslutt :::::::::::::::::::::::::::::::::")
         }
