0131e231a1653f0e6e5679a94d3fdec7f763846b
Millad Dagdoni <milladd@gmail.com>
2021-04-17 21:29:41 +0200

    korrigert språk

--- src/test/kotlin/com/dagdoni/millad/deeplearning/MatriseTest.kt
+++ src/test/kotlin/com/dagdoni/millad/deeplearning/MatriseTest.kt
@@ -11,7 +11,7 @@ import kotlin.math.absoluteValue
 class MatriseTest{
 
     @Test
-    fun `skal kunne hente riktig verdier fra bildematrisen basert på liten kernel størrelse`(){
+    fun `skal kunne hente riktig verdier fra bildematrise basert på liten kernel størrelse`(){
         // GITT
         val mat:Matrix<Double> =    koma.mat[1,1,2,2 end
                                              1,1,2,2 end
@@ -19,7 +19,7 @@ class MatriseTest{
                                              3,3,4,4 ]
 
         // NÅR
-        val matrixFraConvOperasjon = Matrise(mat).hentMatriserFraStridesOperasjon(2)
+        val matriseEtterStrideOperasjon = Matrise(mat).hentMatriserFraStridesOperasjon(2)
 
         // SÅ
         val forventet1:Matrix<Double> =   koma.mat[1,1 end
@@ -31,24 +31,20 @@ class MatriseTest{
         val forventet4:Matrix<Double> =   koma.mat[4,4 end
                                                   4,4]
 
-        assertMatrixEquals(forventet1,matrixFraConvOperasjon.first())
-        assertMatrixEquals(forventet2, matrixFraConvOperasjon[1])
-        assertMatrixEquals(forventet3, matrixFraConvOperasjon[2])
-        assertMatrixEquals(forventet4, matrixFraConvOperasjon[3])
+        assertMatrixEquals(forventet1,matriseEtterStrideOperasjon.first())
+        assertMatrixEquals(forventet2, matriseEtterStrideOperasjon[1])
+        assertMatrixEquals(forventet3, matriseEtterStrideOperasjon[2])
+        assertMatrixEquals(forventet4, matriseEtterStrideOperasjon[3])
     }
 
     @Test
     fun `skal kunne opprette riktig resultat conv operasjon for vertikal ganger vertikal kernet`(){
-        // GITT
         val mat:Matrix<Double> =    koma.mat[0,1,1,0 end
                                             0,1,1,0 end
                                             0,1,1,0 end
                                             0,1,1,0]
 
-        // NÅR
         val matrixFraConvOperasjon:Matrix<Double> = Matrise(mat).conv(Matrise.hentVertikalKernel(2))
-
-        // SÅ
 
         assertThat(matrixFraConvOperasjon.get(0,0).absoluteValue).isEqualTo(2.0)
     }
@@ -56,16 +52,13 @@ class MatriseTest{
 
     @Test
     fun `skal kunne opprette riktig resultat conv operasjon for horisontal ganger vertikal kernet`(){
-        // GITT
+
         val mat:Matrix<Double> =    koma.mat[0,1,1,0 end
                                             0,1,1,0 end
                                             0,1,1,0 end
                                             0,1,1,0]
 
-        // NÅR
         val matrixFraConvOperasjon:Matrix<Double> = Matrise(mat).conv(Matrise.hentHorizontalKernel(2))
-
-        // SÅ
 
         assertThat(matrixFraConvOperasjon.get(0,0).absoluteValue).isEqualTo(4.0)
     }
@@ -86,32 +79,26 @@ class MatriseTest{
 
     @Test
     fun `skal kun returnere 1 om tall er storre enn 0 ellers returner 0`(){
-        // GITT
-        // NÅR
         val tallOver1:Double = Matrise.reluDerivant(2.5)
         val tallUnder0:Double = Matrise.reluDerivant(-1.0)
-        // SÅ
+
         assertThat(tallOver1).isEqualTo(1.0)
         assertThat(tallUnder0).isEqualTo(0.0)
     }
 
     @Test
     fun `skal kun returnere positive tall fra relu operasjon`(){
-        // GITT
-         // NÅR
         val talletNull:Double = Matrise.relu(0.0)
         val talletEn:Double = Matrise.relu(1.0)
-        // SÅ
+
         assertThat(talletNull).isEqualTo(0.0)
         assertThat(talletEn).isEqualTo(1.0)
     }
 
     @Test
     fun `skal kunne opprette et kernel og at kernal har en horizontal linje med tallet 1 omringet ellers med 0`(){
-        // GITT
         val kernelMatrise:Matrix<Double> = Matrise.hentHorizontalKernel(3)
 
-        // SÅ
         assertThat(kernelMatrise.get(0,0)).isEqualTo(0.0)
         assertThat(kernelMatrise.get(1,2)).isEqualTo(1.0)
         assertThat(kernelMatrise.get(1,0)).isEqualTo(1.0)
@@ -123,11 +110,8 @@ class MatriseTest{
 
     @Test
     fun `skal kunne opprette et kernel og at kernal har en vertikal linje med tallet 1 omringet ellers med 0`(){
-
-        //NÅR
         val kernelMatrise:Matrix<Double> = Matrise.hentVertikalKernel(3)
 
-        // SÅ
         assertThat(kernelMatrise.get(0,1)).isEqualTo(1.0)
         assertThat(kernelMatrise.get(1,1)).isEqualTo(1.0)
         assertThat(kernelMatrise.get(2,1)).isEqualTo(1.0)
